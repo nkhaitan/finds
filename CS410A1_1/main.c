fd3317e38a92c542abbe515fdefc5ecdaf37cb36
Nilesh Khaitan <nkhaitan@bu.edu>
2014-03-31 01:34:54 -0400

    further refinements

--- CS410A1_1/main.c
+++ CS410A1_1/main.c
@@ -77,23 +77,23 @@ void my_printf(char * format,...)
             c = *format++;
             switch(c)
             {
-                case 'd':
-                case 'u':
-                case 'x':
-                    itoa(buf, c, *((int *) arg++));
-                    p = buf;
-                    while (*p)
-                    {
-                        putchar(*p++);
-                        count++;
-                    }
-                    break;
                 case 's':
                     p = *arg++;
                     if (!p)
                     {
                         p = "(null)";
                     }
+                    while (*p)
+                    {
+                        putchar(*p++);
+                        count++;
+                    }
+                    break;
+                case 'd':
+                case 'u':
+                case 'x':
+                    itoa(buf, c, *((int *) arg++));
+                    p = buf;
                     while (*p)
                     {
                         putchar(*p++);
